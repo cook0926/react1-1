61bd855031f12ae563bd3a06af81d35db22194af
cook0926 <cook0926@naver.com>
2024-05-22 12:34:28 +0900

    0522

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -3,12 +3,20 @@ import logo from "./logo.svg";
 import "./App.css";
 import Example from "./Eaxmple.js";
 import Accommodate from "./0501-2/Accommodate.jsx";
+import LandingPage from "./0522-1/LandingPage.jsx";
+import NumberList from "./0522-2/NumberList.jsx";
+import AttendanceBook from "./0522-2/AttendanceBook.jsx";
+
 
 function App() {
   return (
     <div>
-      <Accommodate />
-      <Example />
+      {/* <Accommodate />
+      <Example /> */}
+      <LandingPage/>
+      <NumberList/>
+      <AttendanceBook/>
+      
     </div>
   );
 }
